d3465fc39314b3c2f446a2ded47af372bc88dd2e
William Pereira <MetalHexx@outlook.com>
2023-02-19 22:50:52 -0500

    Added a trace processor to add missing trace/span tags to traces.

---
+++ src/GatewayApi/Telemetry/TraceProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using OpenTelemetry;
+using static GatewayApi.Telemetry.TelemetryConstants;
+
+namespace GatewayApi.Telemetry
+{
+    /// <summary>
+    /// Trace processor to intercept and enrich traces
+    /// </summary>
+    public class TraceProcessor : BaseProcessor<Activity>
+    {
+        /// <summary>
+        /// Jaeger seems to be missing the trace / span id in the visualizations. 
+        /// So I'm adding them as additional tags by overriding the OnStart
+        /// </summary>
+        /// <param name="data"></param>
+        public override void OnStart(Activity data)
+        {
+            data.SetTag(Trace_Tag, data.TraceId);
+            data.SetTag(Span_Tag, data.SpanId);
+            base.OnStart(data);
+        }
+    }
+}
